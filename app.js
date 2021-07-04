c8e192499a2bfa282a642cb07267c72802443600
olaaltaslaq <olaaltaslaq@gmail.com>
2021-07-04 21:48:11 +0300

    add more

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ let rightImgEl = document.getElementById('rightImg');
 let ulEl = document.getElementById('results');
 
 let mall = [];
+let attempts = 0;
+let maxAttempts = 25;
 function BusImage(imgName){
-  this.imgName = imgName.split('.')[0];
+  this.iName = imgName.split('.')[0];
   this.img = 'images/' + imgName;
   this.votes = 0;
+  this.views = 0;
 
   mall.push(this);
 }
@@ -25,19 +28,77 @@ function randomIndex(){
   return Math.floor(Math.random() * busimgs.length);
 }
 
+let leftIndex;
+let midIndex;
+let rightIndex;
 function renderRandomImg(){
 
-  let leftIndex = randomIndex();
-  let midIndex = randomIndex();
-  let rightIndex = randomIndex();
+  leftIndex = randomIndex();
+  midIndex = randomIndex();
+  rightIndex = randomIndex();
 
-  leftImgEl.setAttribute('sec', mall[leftIndex].img);
-  midImgEl.setAttribute('sec', mall[midIndex].img);
-  rightImgEl.setAttribute('sec', mall[rightIndex].img);
+  while (leftIndex === midIndex || leftIndex === rightIndex || midIndex === rightIndex){
+    leftIndex = randomIndex();
+    midIndex = randomIndex();
+  }
+
+  leftImgEl.setAttribute('src', mall[leftIndex].img);
+  midImgEl.setAttribute('src', mall[midIndex].img);
+  rightImgEl.setAttribute('src', mall[rightIndex].img);
+
+  leftImgEl.setAttribute('alt', mall[leftIndex].iName);
+  midImgEl.setAttribute('alt', mall[midIndex].iName);
+  rightImgEl.setAttribute('alt', mall[rightIndex].iName);
+
+  leftImgEl.setAttribute('title', mall[leftIndex].iName);
+  midImgEl.setAttribute('title', mall[midIndex].iName);
+  rightImgEl.setAttribute('title', mall[rightIndex].iName);
+
+  mall[leftIndex].views++;
+  mall[midIndex].views++;
+  mall[rightIndex].views++;
 
 }
-
+// console.log(mall);
 renderRandomImg();
+
+
+leftImgEl.addEventListener('click', handelClicks);
+midImgEl.addEventListener('click', handelClicks);
+rightImgEl.addEventListener('click', handelClicks);
+
+function handelClicks(event){
+  attempts++;
+  if (attempts <= maxAttempts){
+    let clickedImg = event.target.id;
+
+    if(clickedImg === 'leftImg'){
+      mall[leftIndex].votes++;
+    } else if (clickedImg === 'midImg'){
+      mall[midIndex].votes++;
+    } else if (clickedImg === 'rightImg'){
+      mall[rightIndex].votes++;
+    }
+    renderRandomImg();
+
+    //   console.log(clickedImg);
+    console.log(mall);
+  } else {
+
+    let ulEl = document.getElementById('results');
+    for (let i=0; i<mall.length; i++){
+      let liEl = document.createElement('li');
+      liEl.textContent = `${mall[i].iName} has ${mall[i].votes} votes and ${mall[i].views} views.`;
+      ulEl.appendChild(liEl);
+    }
+
+    leftImgEl.removeEventListener('click', handelClicks);
+    midImgEl.removeEventListener('click', handelClicks);
+    rightImgEl.removeEventListener('click', handelClicks);
+  }
+}
+
+
 
 
 
